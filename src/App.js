f89f841d6c7a65c4444c03ee8506511e1671e9e7
gaia <hugogaia65@gmail.com>
2022-05-10 20:20:23 -0300

    Styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-
+import styled from 'styled-components';
 function App() {
   return (
-    <div className="App">
+    <styled.div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Gaia
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br/> 
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+
       </header>
-    </div>
+      <body>
+        <div>
+          <p>Fodase</p>
+        </div>
+      </body>
+    </styled.div>
   );
 }
 
