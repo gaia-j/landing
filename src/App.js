c89d3f7c368bb37c553100442617131bf784bc05
Hugo Gaia <hugogaia65@gmail.com>
2022-05-12 17:55:35 -0300

    In fact startted

--- src/App.js
+++ src/App.js
@@ -2,12 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 import {AppWrap} from "./styled";
 import {AiFillGithub} from "react-icons/ai"
-const style = { color: "white", fontSize: "1.5em" }
+import {header} from "./styled"
+
+
+const style = { color: "black", fontSize: "1.5em" }
 function App() {
   return (
       <AppWrap>
-          <div>Fodase</div>
-          <AiFillGithub style={style}/>
+          <div className={"holder"}>
+          <div className={"space"}>
+             <div className={"header"}>
+              <div className={"isheader"}>
+                  Header
+              </div>
+            </div>
+          </div>
+          <div className={"space"}>
+             <div className={"desc"}>
+              <div className={"isdesc"}>
+                  Fodase
+                  <br/>
+                  <a className={"titulo"}>Fodase</a>
+              </div>
+             </div>
+          </div>
+          <div className={"space"}>
+             <div className={"exp"}>
+              <div className={"isexp"}>
+                  Exp
+              </div>
+              <br/>
+              <AiFillGithub style={style}/>
+            </div>
+          </div>
+          <div className={"space"}>
+            <div className={"git"}>
+              <div className={"isgit"}>
+                  Git
+              </div>
+              <br/>
+              <AiFillGithub style={style}/>
+            </div>
+          </div>
+          </div>
+
+
       </AppWrap>
       );
 }
--- src/styled.js
+++ src/styled.js
@@ -1,13 +1,94 @@
 import styled from 'styled-components';
+import bg from './bgdiv.png';
+const media ={
+	desktop: '@media(min-width: 1000px)'
+}
 
 export const AppWrap = styled.div`
-width: 100%;
-border: 1px solid red;
-padding-top: 10px;
-background-color: black;
+
+
+
+
+background-color: rgb(19,14,66);
+min-height: 100vh;
+display: flex;
+flex-flow: column nowrap;
+
+font-size: calc(10px + 5vmin);
+
+${media.desktop} {
+padding-top: 20px;
+background-color: rgb(19,14,66);
+min-height: 100vh;
+display: flex;
+flex-flow: column nowrap;
+padding-left:10vmin;
+padding-right:10vmin;
+font-size: calc(10px + 5vmin);
+}
+
+.titulo{
+font-size: 28px;
+
+}
+.holder{
+
+}
+
+.header{
+  background-color: rgb(85,12,136);
+  color: white;
+  padding-left: 1vmin;
+  ${media.desktop}{
+
+  }
+}
+.space{
+${media.desktop}{
+padding-top: 20px;
+}
+}
+.isheader{
+
+}
+.desc{
+  background-color: rgb(63,15,161);
+  color: white;
+  padding-left: 1vmin;
+  justify-content: center;
+  text-align: center;
+  background-image: url(${bg});
+  ${media.desktop}{
+  background-image: url(${bg});
+  border-radius: 20px;
+  }
+}
+.isdesc{
+
+
+}
+.exp{
+  background-color: rgb(222,229,241);
+  color: black;
+  padding-left: 1vmin;
+  text-align: center;
+}
+.isexp{
+
+}
+.git{
+  background-color: rgb(85,12,136);
+  color: white;
+  padding-left: 1vmin;
+  text-align: center;
+}
+.isgit{
+
+}
+
+
 `
 
 
-const media ={
-	desktop: '@media(min-widht: 1000px)'
-}
+
+
